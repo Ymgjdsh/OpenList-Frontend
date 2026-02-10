dfb73ab25cddc98ab9baf9a648a0e6fc632ee07e
doying <doying@example.com>
2026-02-11 01:05:51 +0800

    最终修复：使用 Lucky 暴露的 dpan:5488 作为 API 代理目标

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,6 +1,7 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url)
-  url.hostname = "64bfb110-a64d-489b-919f-ca8186fa4353.cfargotunnel.com"
+  url.hostname = "dpan.ymg.qzz.io"
+  url.port = "5488"
   url.protocol = "https:"
   return fetch(url.toString(), context.request)
 }
